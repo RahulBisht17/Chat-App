71a5b080b738536c3404372434d121a6f2f3eba6
Rahul Bisht <bishtrahul.1704@gmail.com>
2023-09-23 12:01:01 +0530

    build setup,attached firebase,created routes

---
+++ src/pages/Home.js
@@ -0,0 +1,5 @@
+import React from 'react';
+const Home = () => {
+  return <div>Sign in</div>;
+};
+export default Home;
---
+++ src/pages/SignIn.js
@@ -0,0 +1,5 @@
+import React from 'react';
+const SignIn = () => {
+  return <div>Sign in</div>;
+};
+export default SignIn;
